51cc28ca167caaab4a3ceadd38d3c32dbe137489
Alexandre Costa <alexandrexandefc@gmail.com>
2026-06-23 11:35:04 -0300

    chore: Adiciona o SwaggerConfig ao projeto

---
+++ src/main/java/br/com/proenix/toolsChallenge/configuration/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package br.com.proenix.toolsChallenge.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Tools Java Challenge")
+                        .description("O projeto tem como intuito disponibilizar os endpoints de acesso às funcionalidades do Tools Java Challenge.")
+                        .version("1"));
+    }
+}
