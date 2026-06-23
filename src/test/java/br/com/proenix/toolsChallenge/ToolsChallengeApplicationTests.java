cf15331eb26fb3390cfd2fd6c0a20d274298c880
Alexandre Costa <alexandrexandefc@gmail.com>
2026-06-23 10:15:32 -0300

    chore: inicializa a estrutura do projeto

---
+++ src/test/java/br/com/proenix/toolsChallenge/ToolsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.proenix.toolsChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
