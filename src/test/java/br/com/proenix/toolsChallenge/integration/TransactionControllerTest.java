0e60891edcbba7b809d2633df99b372ce09a5ed0
Alexandre Costa <alexandrexandefc@gmail.com>
2026-06-25 01:00:28 -0300

    test: cria teste de integração com validações do método createTransaction

--- src/test/java/br/com/proenix/toolsChallenge/integration/TransactionControllerTest.java
+++ src/test/java/br/com/proenix/toolsChallenge/integration/TransactionControllerTest.java
@@ -6,10 +6,32 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 
 import br.com.proenix.toolsChallenge.integration.configuration.BaseIntegrationTest;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TransactionControllerTest extends BaseIntegrationTest {
+    private static final String FUTURE_DATE = LocalDateTime.now().plusHours(1).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+    private static final String PAST_DATE = LocalDateTime.now().minusDays(1).format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+
+    private String validBody() {
+        return """
+                {
+                  "transactionId": "1234567890",
+                  "card": "1234567890123456",
+                  "description": {
+                    "value": "100.00",
+                    "date": "%s",
+                    "establishment": "Loja Teste"
+                  },
+                  "paymentMethod": {
+                    "paymentType": "CASH",
+                    "installments": 1
+                  }
+                }
+                """.formatted(FUTURE_DATE);
+    }
 
     @Test
     @DisplayName("Should create a transaction successfully")
@@ -30,8 +52,8 @@ public class TransactionControllerTest extends BaseIntegrationTest {
                             "installments": 1
                           }
                         }
-                        """.formatted(java.time.LocalDateTime.now().plusHours(1)
-                        .format(java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))))
+                        """.formatted(LocalDateTime.now().plusHours(1)
+                        .format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))))
                 .when()
                 .post("/api/v1/transaction")
                 .then()
@@ -48,20 +70,20 @@ public class TransactionControllerTest extends BaseIntegrationTest {
                 .contentType("application/json")
                 .body("""
                         {
-                          "transactionId": "1234567890",
-                          "card": "1234567890123456",
+                          "transactionId": "102030405066",
+                          "card": "5353276659629461",
                           "description": {
-                            "value": "1500.00",
+                            "value": 10050.50,
                             "date": "%s",
                             "establishment": "Loja Teste"
                           },
                           "paymentMethod": {
                             "paymentType": "CASH",
-                            "installments": 1
+                            "installments": "1"
                           }
                         }
-                        """.formatted(java.time.LocalDateTime.now().plusHours(1)
-                        .format(java.time.format.DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))))
+                        """.formatted(LocalDateTime.now().plusHours(1)
+                        .format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"))))
                 .when()
                 .post("/api/v1/transaction")
                 .then()
@@ -69,5 +91,189 @@ public class TransactionControllerTest extends BaseIntegrationTest {
                 .body("description.authorizationCode",nullValue())
                 .body("description.nsu",nullValue())
                 .body("description.transactionStatus",equalTo("DENIED"));
+    }
+
+    @Test
+    @DisplayName("Should return 400 when transactionId is blank")
+    void shouldReturn400WhenTransactionIdIsBlank() {
+        given()
+                .contentType("application/json")
+                .body(validBody().replace("\"1234567890\"","\"\""))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when card is blank")
+    void shouldReturn400WhenCardIsBlank() {
+        given()
+                .contentType("application/json")
+                .body(validBody().replace("\"1234567890123456\"","\"\""))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when description is null")
+    void shouldReturn400WhenDescriptionIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "transactionId": "1234567890",
+                          "card": "1234567890123456",
+                          "description": null,
+                          "paymentMethod": { "paymentType": "CASH", "installments": 1 }
+                        }
+                        """)
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when description value is null")
+    void shouldReturn400WhenDescriptionValueIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "transactionId": "1234567890",
+                          "card": "1234567890123456",
+                          "description": {
+                            "value": null,
+                            "date": "%s",
+                            "establishment": "Loja Teste"
+                          },
+                          "paymentMethod": { "paymentType": "CASH", "installments": 1 }
+                        }
+                        """.formatted(FUTURE_DATE))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when description value is negative")
+    void shouldReturn400WhenDescriptionValueIsNegative() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "transactionId": "1234567890",
+                          "card": "1234567890123456",
+                          "description": {
+                            "value": "-10.00",
+                            "date": "%s",
+                            "establishment": "Loja Teste"
+                          },
+                          "paymentMethod": { "paymentType": "CASH", "installments": 1 }
+                        }
+                        """.formatted(FUTURE_DATE))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when description date is in the past")
+    void shouldReturn400WhenDescriptionDateIsInThePast() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "transactionId": "1234567890",
+                          "card": "1234567890123456",
+                          "description": {
+                            "value": "100.00",
+                            "date": "%s",
+                            "establishment": "Loja Teste"
+                          },
+                          "paymentMethod": { "paymentType": "CASH", "installments": 1 }
+                        }
+                        """.formatted(PAST_DATE))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when establishment is blank")
+    void shouldReturn400WhenEstablishmentIsBlank() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "transactionId": "1234567890",
+                          "card": "1234567890123456",
+                          "description": {
+                            "value": "100.00",
+                            "date": "%s",
+                            "establishment": ""
+                          },
+                          "paymentMethod": { "paymentType": "CASH", "installments": 1 }
+                        }
+                        """.formatted(FUTURE_DATE))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when paymentMethod is null")
+    void shouldReturn400WhenPaymentMethodIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "transactionId": "1234567890",
+                          "card": "1234567890123456",
+                          "description": {
+                            "value": "100.00",
+                            "date": "%s",
+                            "establishment": "Loja Teste"
+                          },
+                          "paymentMethod": null
+                        }
+                        """.formatted(FUTURE_DATE))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when paymentType is invalid")
+    void shouldReturn400WhenPaymentTypeIsInvalid() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "transactionId": "1234567890",
+                          "card": "1234567890123456",
+                          "description": {
+                            "value": "100.00",
+                            "date": "%s",
+                            "establishment": "Loja Teste"
+                          },
+                          "paymentMethod": { "paymentType": "INVALID", "installments": 1 }
+                        }
+                        """.formatted(FUTURE_DATE))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
+    }
+
+    @Test
+    @DisplayName("Should return 400 when installments is null")
+    void shouldReturn400WhenInstallmentsIsNull() {
+        given()
+                .contentType("application/json")
+                .body("""
+                        {
+                          "transactionId": "1234567890",
+                          "card": "1234567890123456",
+                          "description": {
+                            "value": "100.00",
+                            "date": "%s",
+                            "establishment": "Loja Teste"
+                          },
+                          "paymentMethod": { "paymentType": "CASH", "installments": null }
+                        }
+                        """.formatted(FUTURE_DATE))
+                .when().post("/api/v1/transaction")
+                .then().statusCode(400);
     }
 }
